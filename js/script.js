d4160e7fc40f8c416b7a6a78f21333c8e264cc79
Zizoubrown <zizumoha2015@gmail.com>
2020-05-18 17:01:34 +0300

    add front-end logic

--- js/script.js
+++ js/script.js
@@ -60,3 +60,35 @@ Pizza.prototype = {
   		};
 	}
 }
+
+
+const placeOrder = () => {
+	const order = new Pizza(sizeSelector,crustSelector,allToppings);
+	order.pricesForSizes();
+	order.pricesForCrust();
+	order.pricesForToppings();
+	// Calculating the total for one order
+	let oneOrderTotal = crustPrice + sizePrice + toppingsTotal;
+	// Multiplying that with the number of orders choosen by user
+	let numberOfOrders = document.getElementById("number-of-orders").value;
+	let n = parseInt(numberOfOrders);
+	if(numberOfOrders < 1){n = 1};
+	let totalOfAllOrders = n * oneOrderTotal;
+	orderContainer.style.display = "none";
+	cartContainer.style.display = "block";
+	// Cart outputs
+	document.getElementById("choosen-size").innerHTML = sizeName;
+	document.getElementById("choosen-crust").innerHTML = crustName;
+	document.getElementById("choosen-toppings").innerHTML = toppingsName;
+	document.getElementById("orders-choosen").innerHTML = n;
+	document.getElementById("total-amount").innerHTML = totalOfAllOrders;
+};
+
+function finishOrder(){
+	let deliveryOption = document.getElementById("delivery-option");
+	if (deliveryOption.checked){
+		
+		let location = prompt("Enter your location");
+		alert("Your Pizza will be delivered to " + location + " in a few minutes. Thank you");
+	}
+}
